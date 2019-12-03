c32a644e9877b7efa123d2e1674f42294333cfa4
corry010 <cornelis.etta@hotmail.com>
2019-12-03 18:19:42 +0000

    search start

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@
 baguetteBox.run('.gallery', {
   overlayBackgroundColor: "#000"
 });
+
+const $search = $(".search-box");
+
+$search.on("keyUp", function(){
+  const $data = $(".column");
+  for(let i=0; i<$data.length; i++){
+    
+  }
+});
