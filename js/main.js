3bd99cc28d88cda0d2e91f920715f3d3bc5cd348
corry010 <cornelis.etta@hotmail.com>
2019-12-04 10:21:43 +0000

    start up help with function

--- js/main.js
+++ js/main.js
@@ -3,11 +3,13 @@ baguetteBox.run('.gallery', {
   overlayBackgroundColor: "#000"
 });
 
-const $search = $(".search-box");
 
 $search.on("keyUp", function(){
-  const $data = $(".column");
-  for(let i=0; i<$data.length; i++){
-    
-  }
+
+const $search = $(".search-box");
+$search.on("keyup", function(){
+const $data = $(".column a");
+  $data.each(function () {
+    console.log($(this));
+  });
 });
